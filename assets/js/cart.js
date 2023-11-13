8fb9d4fc5bc7f3251c34c25d5cf15e03cfbfd33b
NgCan <118377905+Decan209@users.noreply.github.com>
2023-11-13 23:31:54 +0700

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -10,8 +10,11 @@ const getListCart = () => {
   const noData = document.getElementById("noData");
   const totalPrice = document.getElementById("totalPrice");
 
+  const billProduct = document.getElementById('billProduct');
+
   noData.textContent = "";
   let totalPr = 0;
+  let billHtml = ''
 
   fetch(`http://127.0.0.1:8000/api/cart/${id}`)
     .then((response) => {
@@ -83,13 +86,51 @@ const getListCart = () => {
         </td>
       </tr>
         `;
+
+        billHtml += `
+          <tbody>
+          <tr>
+            <td width="20%">
+              <img
+                src=${element.product.image}
+                width="90"
+              />
+            </td>
+            <td width="50%" class="align-middle">
+              <span class="font-weight-bold">${element.product.name}</span>
+              <div class="product-qty">
+                <span class="d-block">Số lượng: ${element.quantity}</span>
+              </div>
+            </td>
+            <td width="30%" class="align-middle">
+              <div class="text-center">
+                <span class="font-weight-bold">${formatPrice(
+                  (element.product.price -
+                    element.product.price * (element.product.sale / 100)) *
+                    element.quantity
+                )}₫</span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+        `
         });
+
+        billProduct.innerHTML = billHtml;
 
         data.forEach((element) => {
           totalPr += (element.product.price - element.product.price * (element.product.sale / 100)) *element.quantity
         });
 
         totalPrice.textContent = formatPrice(totalPr) +"₫"
+
+        const billTotal = document.getElementById('billTotal');
+        const billName = document.getElementById('billName');
+        const billPhone = document.getElementById('billPhone');
+
+        billName.textContent = localStorage.getItem("userName");
+        billPhone.textContent = localStorage.getItem("userPhone");
+        billTotal.textContent = formatPrice(totalPr) +"₫"
       }
     })
     .catch((error) => {
@@ -121,7 +162,8 @@ const removeProductCart = (id) => {
       while (table.rows.length > 1) {
         table.deleteRow(1);
       }
-      getListCart()
+      document.getElementById("totalPrice").textContent = "0₫"
+      getListCart();
   })
   .catch((error) => {
     console.error("Lỗi:", error);
